fc4d3a54d1717bbb491aa054753b18bb1690c432
Simone Marchisio <marchisio.simone@gmail.com>
2018-03-08 13:42:46 +0100

    User authentication with devise_token_auth Gem.

---
+++ rails/app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ActiveRecord::Base
+    
+    # Include default devise modules. Others available are:
+    # :confirmable, :lockable, :timeoutable and :omniauthable
+    devise :database_authenticatable, :registerable,
+            :recoverable, :rememberable, :trackable, :validatable
+    include DeviseTokenAuth::Concerns::User
+
+    # User validation
+    validates :email, presence: true, uniqueness: true
+    validates :name, :surname, presence: true
+
+    # User method used to check if a user is an admin or a member
+    def admin?
+        self.type == "Admin"
+    end
+end
--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -2,13 +2,16 @@
 require 'api_constraints'
 
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
     # Api definition, it maps routes to controllers into controllers/api, set the data format as JSON and set this path as root (host/api)
     namespace :api, defaults: { format: :json }, path: '/' do
         
         # Scope APIs in different versions of them, the version 1 is the default one
         scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+            # User authentication routes
+            mount_devise_token_auth_for 'User', at: 'auth'
             
 
         end
